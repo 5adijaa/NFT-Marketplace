a9ffabdd7b0a8bd24f7b70cb39d8e1f3053ef107
Khadija T <tal7akhadija@gmail.com>
2021-11-28 22:12:58 +0100

    Create the NFT Smart Contract CryptoWhale.sol

---
+++ src/contracts/CryptoWhale.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Cryptowhale {
+    string public name;
+    string public symbol;
+
+    constructor(){
+        name = 'Cryptowhale';
+        symbol = 'CWHALE';
+    }
+}
