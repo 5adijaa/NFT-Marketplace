3fb1e8ccdfe612f41d46e169c23b9fc2304bb287
Khadija T <tal7akhadija@gmail.com>
2021-12-01 12:57:34 +0100

    Create the balanceOf & ownerOf NFT functions

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -18,6 +18,27 @@ contract ERC721 {
 
     mapping(address => uint) private _OwnedTokensCount;
 
+    /// @notice Count all NFTs assigned to an owner
+    /// @dev NFTs assigned to the zero address are considered invalid, and this
+    ///  function throws for queries about the zero address.
+    /// @param _owner An address for whom to query the balance
+    /// @return The number of NFTs owned by `_owner`, possibly zero
+    function balanceOf(address _owner) external view returns (uint){
+        require(_owner != address(0), 'Owner query for non-existant token');
+        return _OwnedTokensCount[_owner];
+    }
+    
+    // @notice Find the owner of an NFT
+    /// @dev NFTs assigned to zero address are considered invalid, and queries
+    ///  about them do throw.
+    /// @param _tokenId The identifier for an NFT
+    /// @return The address of the owner of the NFT
+    function ownerOf(uint _tokenId) external view returns (address){
+        address owner = _tokenOwner[_tokenId];
+        require(owner != address(0), 'Owner query for non-existant token');
+        return owner;
+    }
+
     function _exists(uint tokenId) internal view returns(bool){
         address owner = _tokenOwner[tokenId];
         return owner !=address(0);
